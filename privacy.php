7e93bac2870d0233d24c9eab83e142737f899596
Morena <onalepeloo@live.com>
2022-05-20 17:13:12 +0200

    refactored the privacy page

--- privacy.php
+++ privacy.php
@@ -12,65 +12,54 @@
       <div class="row">
         <div class="col s12">
         <ul class="collection with-header ">
-        <li class="collection-header purple-text text-darken-1 center"><h4>Privacy Policy</h4>
-        <p class="black-text">This privacy policy sets out how Ithute Motswana uses and protects any information that you give Ithute Motswana when you use this website.</p>
-        <span class="black-text "> Ithute Motswana is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.
-        IM may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from 22 June 2021. <br>
-        <b>What data we collect and why we collect it:</b>
-        </span>
-        </li>
-        <li class="collection-item">Names, date of birth, gender, send, identity documents(Passport)</li>
-        <li class="collection-item">Contact information including email addresses, phones numbers and mobile number</li>
-        <li class="collection-item">demographic information such as postcode, residential address and locations and postal address</li>
-        <li class="collection-item">All required medical examination tests and scans by the chinese scholarship council and  universities</li>
-        <li class="collection-item">Academic documents, supporting documents and any other document required to successfully process each application</li>
-        <li class="collection-item">From time to time, we may also use your information to contact you. We may contact you by email, phone, fax or mail. We may use the information to customise the website to serve you and your interests better.</li>
-
+            <li class="collection-header purple-text text-darken-1 center"><h4>Privacy Policy</h4>
+              <p class="black-text">This privacy policy sets out how Ithute Motswana uses and protects any information that you give Ithute Motswana when you use this website.</p>
+              <span class="black-text "> 
+                  Ithute Motswana is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.
+                  IM may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from 22 June 2021. <br>
+                <b>What data we collect and why we collect it:</b>
+              </span>
+            </li>
+            <li class="collection-item">Names, date of birth, gender, send, identity documents(Passport)</li>
+            <li class="collection-item">Contact information including email addresses, phones numbers and mobile number</li>
+            <li class="collection-item">demographic information such as postcode, residential address and locations and postal address</li>
+            <li class="collection-item">All required medical examination tests and scans by the chinese scholarship council and  universities</li>
+            <li class="collection-item">Academic documents, supporting documents and any other document required to successfully process each application</li>
+            <li class="collection-item">From time to time, we may also use your information to contact you. We may contact you by email, phone, fax or mail. We may use the information to customize the website to serve you and your interests better.</li>
         </ul>
+
           <ul class="collection with-header ">
-           <li class="collection-header purple-text text-darken-1 center"><h4>Who we share your data with</h4>
+            <li class="collection-header purple-text text-darken-1 center"><h4>Who we share your data with</h4>
             <li class="collection-item">Ithute Motswana shares your information with anyone we deem fit to be able to provide the best kind of advice and guidance with your application. Any information transmitted to third parties is for the sole purpose of helping you with your scholarship application process not for monetary gain.</li>
           </ul>
 
           <ul class="collection with-header ">
-           <li class="collection-header purple-text text-darken-1 center"><h4>How long we retain your data</h4>
-            <li class="collection-item">For users that register on our website, we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information. All information related to each user can be deleted if a user decideds to close his or her account with us.</li>
+            <li class="collection-header purple-text text-darken-1 center"><h4>How long we retain your data</h4>
+            <li class="collection-item">For users that register on our website, we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information. All information related to each user can be deleted if a user decides to close his or her account with us.</li>
           </ul>
 
-             <ul class="collection with-header ">
-           <li class="collection-header purple-text text-darken-1 center"><h4>What rights you have over your data</h4>
+          <ul class="collection with-header ">
+            <li class="collection-header purple-text text-darken-1 center"><h4>What rights you have over your data</h4>
             <li class="collection-item">If you have an account on this site, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.</li>
           </ul>
 
-                 <ul class="collection with-header ">
-           <li class="collection-header purple-text text-darken-1 center"><h4>How we protect your data</h4>
-            <li class="collection-item">We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</li>
+          <ul class="collection with-header ">
+            <li class="collection-header purple-text text-darken-1 center"><h4>How we protect your data</h4>
+            <li class="collection-item">We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</li>
           </ul>
-
-
-
         </div>
-
       </div>
     </div>
   </section>
 
-  <!-- Footer -->
-    <?php include "includes/footer.php";?>
+  
+  <!-- Footer -->  
+  <?php include "includes/footer.php";?>
 
-
-
-  <!--Import jQuery before materialize.js-->
+  <!--Import jQuery, materialize.js and the helper functions-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="js/materialize.min.js"></script>
-  <script>
-    $(document).ready(function () {
-      $('.carousel.carousel-slider').carousel({ fullWidth: true });
-      $('.button-collapse').sideNav();
-      $('.modal').modal();
-      $('select').material_select();
-    });
-  </script>
+  <script type="text/javascript" src="js/util.js"></script>
+  
 </body>
-
 </html>
