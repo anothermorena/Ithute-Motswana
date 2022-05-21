b2ed6e607484b6412ba7ae77ffccb665635f7547
Morena <onalepeloo@live.com>
2022-05-21 13:21:35 +0200

    refactored the contacts page

--- clients/bank_details.php
+++ clients/bank_details.php
@@ -3,23 +3,23 @@
     <p>Please all payments should be made to the following account</p>
     <p class="red-text">Please use application invoice number as your reference</p>
      <form action="#">
-            <table class="highlight centered responsive-table">
-                <thead>
-                <tr>
-                    <th>Bank</th>
-                    <th>Branch Code</th>
-                    <th>Account Name</th>
-                    <th>Account Number</th>            
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>First National Bank Botswana</td>
-                    <td>283567</td>
-                    <td>Otsogile Onalepelo</td>
-                    <td>62423308974</td>    
-                </tr>
-                </tbody>
-            </table>
+        <table class="highlight centered responsive-table">
+            <thead>
+            <tr>
+                <th>Bank</th>
+                <th>Branch Code</th>
+                <th>Account Name</th>
+                <th>Account Number</th>            
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>First National Bank Botswana</td>
+                <td>283567</td>
+                <td>Otsogile Onalepelo</td>
+                <td>62423308974</td>    
+            </tr>
+            </tbody>
+        </table>
     </form>
 </center>
--- clients/contacts.php
+++ clients/contacts.php
@@ -1,34 +1,26 @@
 <center>
     <h5>Our Contacts</h5>
     <p>Please connect with us through the channels listed below</p>
-   
      <form action="#">
-            <table class="highlight centered responsive-table">
-                <thead>
+        <table class="highlight centered responsive-table">
+            <thead>
                 <tr>
-                    <th>Call</th>
-                    <th>Whatsapp</th>
-                    <th>Email</th>
-                    <th>Facebook</th>
-                    <th>Instagram</th>
-                   
-                   
+                   <th>Call</th>
+                   <th>Whatsapp</th>
+                   <th>Email</th>
+                   <th>Facebook</th>
+                   <th>Instagram</th>  
                 </tr>
-                </thead>
-
+            </thead>
                 <tbody>
-               
                 <tr>
-                    <td>+26772608067 / +26775123837</td>
-                    <td>+8613281289043 / +26772608067 / +26775123837</td>
-                    <td>service@ithutemotswana.com</td>
-                    <td>Ithute Motswana</td>    
-                    <td>@ithute_motswana</td>    
+                  <td>+26772608067</td>
+                  <td>+8613281289043 / +26772608067</td>
+                  <td>service@ithutemotswana.com</td>
+                  <td>Ithute Motswana</td>    
+                  <td>@ithute_motswana</td>    
                 </tr>
-          
-        
-                </tbody>
-            </table>
-             </form>
-
+            </tbody>
+        </table>
+    </form>
 </center>
