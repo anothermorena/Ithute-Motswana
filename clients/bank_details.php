749e3af01ec93d994681c26dacf0edff0466dd65
Morena <onalepeloo@live.com>
2022-05-21 13:17:41 +0200

    refactored bank details page

--- clients/bank_details.php
+++ clients/bank_details.php
@@ -1,8 +1,7 @@
 <center>
     <h5>Our Banking Details</h5>
-    <p>Please all payments should be made to the follwing account</p>
-    <p class="red-text">Please use application invoice number as your refference</p>
-   
+    <p>Please all payments should be made to the following account</p>
+    <p class="red-text">Please use application invoice number as your reference</p>
      <form action="#">
             <table class="highlight centered responsive-table">
                 <thead>
@@ -10,24 +9,17 @@
                     <th>Bank</th>
                     <th>Branch Code</th>
                     <th>Account Name</th>
-                    <th>Account Number</th>           
-                   
+                    <th>Account Number</th>            
                 </tr>
                 </thead>
-
                 <tbody>
-               
                 <tr>
                     <td>First National Bank Botswana</td>
                     <td>283567</td>
                     <td>Otsogile Onalepelo</td>
                     <td>62423308974</td>    
                 </tr>
-          
-        
                 </tbody>
             </table>
-        
     </form>
-
 </center>
